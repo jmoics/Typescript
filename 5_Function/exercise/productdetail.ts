02f31c347d20b850ae6686999328d4327219a287
Jorge Cueva <jmoics@gmail.com>
2020-05-18 00:21:00 -0500

    added functions exercise 2.

--- 5_Function/exercise/productdetail.ts
+++ 5_Function/exercise/productdetail.ts
@@ -7,22 +7,26 @@ if (product.productAvailable) {
 } else {
     document.getElementById('pAvailable').innerHTML = "OutOfStock";
 }
+document.getElementById('pName').innerHTML = product.productName;
 document.getElementById('productDescription').innerHTML = product.productDescription;
 document.getElementById('gold').innerHTML = product.productPrice.gold;
 document.getElementById('pinkgold').innerHTML = product.productPrice.pink;
 document.getElementById('silver').innerHTML = product.productPrice.silver;
 
 
-function getMobileByColor(color: string) {
-    prodName = product.productName;
+function getMobileByColor(color: string, image: any) {
     if (color === 'gold') {
         document.getElementById('pPrice').innerHTML = ""+product.productPrice.gold;
         document.getElementById('pDiscount').innerHTML = ""+15;
+        image.src = '../../images/SamsungGalaxy_Gold.jpg'
     } else if (color === 'pink') {
         document.getElementById('pPrice').innerHTML = ""+product.productPrice.pink;
         document.getElementById('pDiscount').innerHTML = ""+10;
+        image.src = '../../images/SamsungGalaxy_Pink.jpg'
     } else {
         document.getElementById('pPrice').innerHTML = ""+product.productPrice.silver;
         document.getElementById('pDiscount').innerHTML = ""+8;
+        image.src = '../../images/samsung_edge_silver.jpg'
     }
+
 }
